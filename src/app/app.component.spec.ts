e3d0cd0e6f3182b3edfa4f954903e29191f37ef3
aleksandra hulecka <aleksandra.a.hulecka@gmail.com>
2019-07-10 16:29:13 +0200

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('project');
+    expect(app.title).toEqual('Project');
   });
 
   it('should render title in a h1 tag', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import json from '../assets/exams.json';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project';
+  title = 'Test psychologiczny';
+
+  constructor() {
+   this.title = json.exams.exam[0].name;
+  console.log(json);
+
+  }
+
+//for.exams[0]questions[0];
+//  else.exams[1]questions[1];
+
 }
+
+//console.log(angular.toJson($scope.exams.json));
+
+// $http.get('assets/exams.json');
