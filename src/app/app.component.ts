72129434df4bc1fa237c6bbbcfbc76aa4ca1666d
BROSTUDIO <marcin@brostudio.pl>
2019-07-22 18:01:31 +0200

    Logic for changing JSON state with checked radio buttons There is a need to work to complete "submitExam()" method which is designed to get "this.questions" property with all valid "checked" = TRUE or FALSE variables filled during HTML form completion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,37 @@
 import { Component } from '@angular/core';
 import json from '../assets/exams.json';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Test psychologiczny';
+  title:string = '';
   questions = {};
 
   constructor() {
    this.title = json.exams.exam[0].name;
    this.questions = json.exams.exam[0].questions;
-  console.log(this.questions);
-
   }
 
-//for.exams[0]questions[0];
-//  else.exams[1]questions[1];
+  checkAnswer(idQuestion:number, idAnswer:number): void
+  {
+    let answers = this.questions[idQuestion].answers;
+    for(let i=0; i<answers.length; i++) {
+      answers[i].checked = (i == idAnswer) ? true : false;
+    }
+
+    console.log(this.questions);
+  }
+
+  submitExam(): void
+  {
+    // This method loop through JSON (this.questions) updated by checked radio buttons.
+    // Goal of this method is to sum question points and provide result of person personality
+
+    console.log(this.questions);
+  }
 
 }
-
-//console.log(angular.toJson($scope.exams.json));
-
-// $http.get('assets/exams.json');
